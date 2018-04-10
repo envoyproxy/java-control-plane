5c648cbd035400f5cc9f04e8c993584c2210f2d2
Joey Bratton <jbratton@salesforce.com>
2018-04-10 08:59:27 -0400

    test: Initial ADS integration test
    Signed-off-by: Joey Bratton <jbratton@salesforce.com>

---
+++ server/src/test/java/io/envoyproxy/controlplane/server/TestSnapshots.java
@@ -0,0 +1,34 @@
+package io.envoyproxy.controlplane.server;
+
+import envoy.api.v2.Cds.Cluster;
+import envoy.api.v2.Lds.Listener;
+import envoy.api.v2.Rds.RouteConfiguration;
+import io.envoyproxy.controlplane.cache.Snapshot;
+import io.envoyproxy.controlplane.cache.TestResources;
+import org.testcontainers.shaded.com.google.common.collect.ImmutableList;
+
+class TestSnapshots {
+
+  static Snapshot createSnapshot(
+      String clusterName,
+      String endpointAddress,
+      int endpointPort,
+      String listenerName,
+      int listenerPort,
+      String routeName,
+      String version) {
+
+    Cluster cluster = TestResources.createCluster(clusterName, endpointAddress, endpointPort);
+    Listener listener = TestResources.createListener(listenerName, listenerPort, routeName);
+    RouteConfiguration route = TestResources.createRoute(routeName, clusterName);
+
+    return Snapshot.create(
+        ImmutableList.of(cluster),
+        ImmutableList.of(),
+        ImmutableList.of(listener),
+        ImmutableList.of(route),
+        version);
+  }
+
+  private TestSnapshots() { }
+}
---
+++ server/src/test/resources/envoy/launch_envoy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -eu
+
+HOST_IP=$(sh /usr/local/bin/host_ip.sh)
+HOST_PORT=$1
+
+CONFIG=$(cat $2)
+CONFIG_DIR=$(mktemp -d)
+CONFIG_FILE="$CONFIG_DIR/envoy.yaml"
+
+echo "${CONFIG}" | sed -e "s/HOST_IP/${HOST_IP}/g" -e "s/HOST_PORT/${HOST_PORT}/g" > "${CONFIG_FILE}"
+cat "${CONFIG_FILE}"
+
+shift 2
+/usr/local/bin/envoy --drain-time-s 1 --v2-config-only -c "${CONFIG_FILE}" "$@"
+
+rm -rf "${CONFIG_DIR}"
