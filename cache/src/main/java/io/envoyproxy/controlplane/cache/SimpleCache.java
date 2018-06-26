d611544e021b4d92f39fc21e3ef336c227e59b8f
Snow Pettersen <snowp@squareup.com>
2018-06-25 22:53:09 -0400

    cache: remove cache lock in cancel callback
    The CacheStatusInfo#cancel method is already guarded by a mutex, so the
    additional locking on the entire SnapshotCache seems to be unnecessary.
    Since we're only mutating the CacheStatusInfo and not the SnapshotCache,
    the write lock seems like overkill.
    
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- cache/src/main/java/io/envoyproxy/controlplane/cache/SimpleCache.java
+++ cache/src/main/java/io/envoyproxy/controlplane/cache/SimpleCache.java
@@ -110,15 +110,7 @@ public class SimpleCache<T> implements SnapshotCache<T> {
 
         status.setWatch(watchId, watch);
 
-        watch.setStop(() -> {
-          writeLock.lock();
-
-          try {
-            status.removeWatch(watchId);
-          } finally {
-            writeLock.unlock();
-          }
-        });
+        watch.setStop(() -> status.removeWatch(watchId));
 
         return watch;
       }
