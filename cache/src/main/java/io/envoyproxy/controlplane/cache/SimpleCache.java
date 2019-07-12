f127e26376eb3b193e67f248730924f9f0c7f038
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2019-07-12 11:33:41 -0300

    make setSnapshot synchronized
    Signed-off-by: Sebastian Schepens <sebastian.schepens@mercadolibre.com>

--- cache/src/main/java/io/envoyproxy/controlplane/cache/SimpleCache.java
+++ cache/src/main/java/io/envoyproxy/controlplane/cache/SimpleCache.java
@@ -196,7 +196,7 @@ public class SimpleCache<T> implements SnapshotCache<T> {
    * {@inheritDoc}
    */
   @Override
-  public void setSnapshot(T group, Snapshot snapshot) {
+  public synchronized void setSnapshot(T group, Snapshot snapshot) {
     // we take a writeLock to prevent watches from being created while we update the snapshot
     CacheStatusInfo<T> status;
     writeLock.lock();
