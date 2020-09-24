d61954420d414ec6206755bb68afd746bc6ef509
Krzysztof Słonka <slonka@users.noreply.github.com>
2020-09-24 10:17:28 +0200

    Fix flapping warming cluster integration tests (#145)
    * Try to fix flapping warming cluster integration tests
    
    Signed-off-by: slonka <slonka@users.noreply.github.com>
    
    * Remove delay, add assertion on 503
    
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- server/src/test/java/io/envoyproxy/controlplane/server/V2DiscoveryServerAdsWarmingClusterIT.java
+++ server/src/test/java/io/envoyproxy/controlplane/server/V2DiscoveryServerAdsWarmingClusterIT.java
@@ -1,6 +1,5 @@
 package io.envoyproxy.controlplane.server;
 
-import static io.envoyproxy.controlplane.server.V2TestSnapshots.createSnapshot;
 import static io.envoyproxy.envoy.api.v2.core.ApiVersion.V2;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,9 +7,7 @@ import static org.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.containsString;
 
 import com.google.protobuf.util.Durations;
-import io.envoyproxy.controlplane.cache.CacheStatusInfo;
 import io.envoyproxy.controlplane.cache.NodeGroup;
-import io.envoyproxy.controlplane.cache.Resources;
 import io.envoyproxy.controlplane.cache.TestResources;
 import io.envoyproxy.controlplane.cache.v2.SimpleCache;
 import io.envoyproxy.controlplane.cache.v2.Snapshot;
@@ -26,10 +23,7 @@ import io.envoyproxy.envoy.api.v2.core.Http2ProtocolOptions;
 import io.envoyproxy.envoy.api.v2.core.Node;
 import io.grpc.netty.NettyServerBuilder;
 import io.restassured.http.ContentType;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -43,7 +37,7 @@ public class V2DiscoveryServerAdsWarmingClusterIT {
   private static final String CONFIG = "envoy/ads.v2.config.yaml";
   private static final String GROUP = "key";
   private static final Integer LISTENER_PORT = 10000;
-  private static final CustomCache<String> cache = new CustomCache<>(new NodeGroup<String>() {
+  private static final SimpleCache<String> cache = new SimpleCache<>(new NodeGroup<String>() {
     @Override public String hash(Node node) {
       return GROUP;
     }
@@ -60,7 +54,6 @@ public class V2DiscoveryServerAdsWarmingClusterIT {
   private static final NettyGrpcServerRule ADS = new NettyGrpcServerRule() {
     @Override
     protected void configureServerBuilder(NettyServerBuilder builder) {
-      ExecutorService executorService = Executors.newSingleThreadExecutor();
       final DiscoveryServerCallbacks callbacks = new DiscoveryServerCallbacks() {
             @Override
             public void onStreamOpen(long streamId, String typeUrl) {
@@ -82,7 +75,6 @@ public class V2DiscoveryServerAdsWarmingClusterIT {
             public void onStreamResponse(long streamId, DiscoveryRequest request, DiscoveryResponse response) {
               // Here we update a Snapshot with working cluster, but we change only CDS version, not EDS version.
               // This change allows to test if EDS will be sent anyway after CDS was sent.
-              createSnapshotWithWorkingClusterWithTheSameEdsVersion(request, executorService);
               onStreamResponseLatch.countDown();
             }
           };
@@ -134,26 +126,31 @@ public class V2DiscoveryServerAdsWarmingClusterIT {
     await().atMost(5, TimeUnit.SECONDS).ignoreExceptions().untilAsserted(
         () -> given().baseUri(baseUri).contentType(ContentType.TEXT)
             .when().get("/")
+            .then().statusCode(503));
+
+    // Here we update a Snapshot with working cluster, but we change only CDS version, not EDS version.
+    // This change allows to test if EDS will be sent anyway after CDS was sent.
+    createSnapshotWithWorkingClusterWithTheSameEdsVersion();
+
+    await().atMost(5, TimeUnit.SECONDS).ignoreExceptions().untilAsserted(
+        () -> given().baseUri(baseUri).contentType(ContentType.TEXT)
+            .when().get("/")
             .then().statusCode(200)
             .and().body(containsString(UPSTREAM.response)));
   }
 
-  private static void createSnapshotWithWorkingClusterWithTheSameEdsVersion(DiscoveryRequest request,
-                                                                            ExecutorService executorService) {
-    if (request.getTypeUrl().equals(Resources.V2.CLUSTER_TYPE_URL)) {
-      executorService.submit(() -> cache.setSnapshot(
-          GROUP,
-          createSnapshot(true,
-              "upstream",
-              UPSTREAM.ipAddress(),
-              EchoContainer.PORT,
-              "listener0",
-              LISTENER_PORT,
-              "route0",
-              "2"))
-      );
-    }
+  private static void createSnapshotWithWorkingClusterWithTheSameEdsVersion() {
+    cache.setSnapshot(GROUP,
+        V2TestSnapshots.createSnapshot(true,
+            "upstream",
+            UPSTREAM.ipAddress(),
+            EchoContainer.PORT,
+            "listener0",
+            LISTENER_PORT,
+            "route0",
+            "2"));
   }
+
 
   private static Snapshot createSnapshotWithNotWorkingCluster(boolean ads,
                                                               String clusterName,
@@ -197,34 +194,21 @@ public class V2DiscoveryServerAdsWarmingClusterIT {
   }
 
 
-  /**
-   * Code has been copied from io.envoyproxy.controlplane.cache.SimpleCache to show specific case when
-   * Envoy might stuck with warming cluster. Class has changed lines from method respondWithSpecificOrder which are
-   * responsible for responding for watches. Because to reproduce this problem we need a lot of connected Envoy's and
-   * changes to snapshot it is easier to reproduce this way.
+  /*
+   * In the previous versions of this tests we had a copied SimpleCache with respondWithSpecificOrder removed.
+   * With new versions of Envoy hitting this edge-case became highly improbable.
+   * Now this test checks only if a CDS change will also send EDS.
+   * 1. Envoy connects to control-plane
+   * 2. Snapshot already exists in control-plane <- other instance share same group
+   * 3. Control-plane respond with CDS in createWatch method
+   * 4. There is snapshot update which change CDS and EDS versions
+   * 5. Envoy sends EDS request
+   * 6. Control-plane respond with EDS in createWatch method
+   * 7. Envoy resume CDS and EDS requests.
+   * 8. Envoy sends request CDS
+   * 9. Control plane respond with CDS in createWatch method
+   * 10. Envoy sends EDS requests
+   * 11. Control plane doesn't respond because version hasn't changed
+   * 12. Cluster of service stays in warming phase
    */
-  static class CustomCache<T> extends SimpleCache<T> {
-
-    public CustomCache(NodeGroup<T> groups) {
-      super(groups);
-    }
-
-    @Override
-    protected void respondWithSpecificOrder(T group, Snapshot snapshot,
-        ConcurrentMap<Resources.ResourceType, CacheStatusInfo<T>> status) {
-      // This code has been removed to show specific case which is hard to reproduce in integration test:
-      //      1. Envoy connects to control-plane
-      //      2. Snapshot already exists in control-plane <- other instance share same group
-      //      3. Control-plane respond with CDS in createWatch method
-      //      4. There is snapshot update which change CDS and EDS versions
-      //      5. Envoy sends EDS request
-      //      6. Control-plane respond with EDS in createWatch method
-      //      7. Envoy resume CDS and EDS requests.
-      //      8. Envoy sends request CDS
-      //      9. Control plane respond with CDS in createWatch method
-      //      10. Envoy sends EDS requests
-      //      11. Control plane doesn't respond because version hasn't changed
-      //      12. Cluster of service stays in warming phase
-    }
-  }
 }
