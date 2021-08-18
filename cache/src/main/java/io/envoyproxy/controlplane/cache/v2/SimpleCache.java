b727a8eea754b2311bca323e26d1ef062f28b2c8
Michael Li <mli@confluent.io>
2021-08-17 21:13:20 -0700

    updated pom

--- cache/src/main/java/io/envoyproxy/controlplane/cache/v2/SimpleCache.java
+++
@@ -1,9 +0,0 @@
-package io.envoyproxy.controlplane.cache.v2;
-
-import io.envoyproxy.controlplane.cache.NodeGroup;
-
-public class SimpleCache<T> extends io.envoyproxy.controlplane.cache.SimpleCache<T, Snapshot> {
-  public SimpleCache(NodeGroup<T> nodeGroup) {
-    super(nodeGroup);
-  }
-}
