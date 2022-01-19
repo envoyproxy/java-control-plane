fd900461cf22af17914be563ae0bbd9ba74bde19
Sagar Shah <sagarcs1991@gmail.com>
2022-01-18 23:40:27 -0800

    Add opentelemetry to tools/API_SHAS
    Signed-off-by: Sagar Shah <sagarcs1991@gmail.com>

--- tools/update-api.sh
+++ tools/update-api.sh
@@ -50,13 +50,17 @@ curl -sL https://github.com/census-instrumentation/opencensus-proto/archive/v${O
 mkdir -p "${protodir}/opencensus/proto"
 cp -r opencensus-proto-*/src/opencensus/proto/* "${protodir}/opencensus/proto"
 
+curl -sL https://github.com/open-telemetry/opentelemetry-proto/archive/refs/tags/v${OPENTELEMETRY_VERSION}.tar.gz | tar xz --include="*.proto"
+mkdir -p "${protodir}/opentelemetry/proto"
+cp -r opentelemetry-proto-*/opentelemetry/proto/* "${protodir}/opentelemetry/proto"
+
 curl -sL https://github.com/prometheus/client_model/archive/${PROMETHEUS_SHA}.tar.gz | tar xz --include="*.proto"
 cp client_model-*/metrics.proto "${protodir}"
 
-curl -sL https://github.com/cncf/udpa/archive/${UDPA_SHA}.tar.gz | tar xz --include="*.proto"
+curl -sL https://github.com/cncf/xds/archive/${UDPA_SHA}.tar.gz | tar xz --include="*.proto"
 mkdir -p "${protodir}/udpa"
 mkdir -p "${protodir}/xds"
-cp -r udpa-*/udpa/* "${protodir}/udpa"
-cp -r udpa-*/xds/* "${protodir}/xds"
+cp -r xds-*/udpa/* "${protodir}/udpa"
+cp -r xds-*/xds/* "${protodir}/xds"
 
 popd >/dev/null
