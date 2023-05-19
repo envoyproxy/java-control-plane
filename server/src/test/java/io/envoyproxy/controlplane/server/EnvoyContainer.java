b9327f23647aa7c5f824db508114306aa891fd21
rulex123 <29862113+rulex123@users.noreply.github.com>
2023-05-19 16:03:48 +0200

    Switch to official release docker image for test container
    Signed-off-by: rulex123 <29862113+rulex123@users.noreply.github.com>

--- server/src/test/java/io/envoyproxy/controlplane/server/EnvoyContainer.java
+++ server/src/test/java/io/envoyproxy/controlplane/server/EnvoyContainer.java
@@ -24,7 +24,7 @@ class EnvoyContainer extends GenericContainer<EnvoyContainer> {
   EnvoyContainer(String config, Supplier<Integer> controlPlanePortSupplier) {
     // this version is changed automatically by /tools/update-sha.sh:57
     // if you change it make sure to reflect changes there
-    super("envoyproxy/envoy-dev:4b5f5474f35763423684c0fe25c99cc7b2a01fcf");
+    super("envoyproxy/envoy:v1.25.5");
     this.config = config;
     this.controlPlanePortSupplier = controlPlanePortSupplier;
   }
